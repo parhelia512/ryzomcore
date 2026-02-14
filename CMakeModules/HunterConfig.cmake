c2e8e3208e3c78822a2549340512019370692419
kaetemi <jan.boon@kaetemi.be>
2026-02-15 00:19:51 +0800

    Disable macOS CI for now

--- CMakeModules/HunterConfig.cmake
+++ CMakeModules/HunterConfig.cmake
@@ -6,11 +6,6 @@ hunter_config(luabind
   SHA1 "1dfabfa89ee72066118e4e28e797830e118d2a9b"
   )
 
-hunter_config(PNG
-  VERSION "1.6.50"
-  URL "https://github.com/cpp-pm/libpng/archive/v1.6.50.tar.gz"
-  SHA1 "61aa394ee5fa94735025df1c94a09b38184be413")
-
 hunter_config(OpenAL
   VERSION "1.20.1"
   URL "https://github.com/kcat/openal-soft/archive/openal-soft-1.20.1.tar.gz"
